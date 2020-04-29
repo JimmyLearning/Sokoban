0b69651a00ff55874763bbf77ed715b241c0bd26
Z3 <doumjun@126.com>
2020-04-29 09:22:00 +0800

    template

---
+++ src/main/java/com/demo/game/util/creator/TestMain.java
@@ -0,0 +1,15 @@
+/**
+ * Jimmy
+ */
+package com.demo.game.util.creator;
+
+import java.io.IOException;
+
+/** Jimmy */
+public class TestMain {
+
+    public static void main(String[] args) throws IOException {
+        ClassCreator.javaBuild("MyTest5");
+    }
+
+}
